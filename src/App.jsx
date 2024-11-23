c293dd08818958840cdd6b922d9de6f0cae98923
Shaban Samani <shabansamani@gmail.com>
2024-11-23 17:30:17 -0500

    JobPage, AddJobPage, and api for adding/deleting jobs

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,43 @@ import MainLayout from "./layouts/MainLayout";
 import HomePage from "./pages/HomePage";
 import JobsPage from "./pages/JobsPage";
 import NotFoundPage from "./pages/NotFoundPage";
+import JobPage, { jobLoader } from "./pages/JobPage";
+import AddJobPage from "./pages/AddJobPage";
+
+const addJob = async (newJob) => {
+  const res = await fetch("/api/jobs", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(newJob),
+  });
+  return;
+};
+
+const deleteJob = async (jobId) => {
+  const res = await fetch(`/api/jobs/${jobId}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  return;
+};
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<MainLayout />}>
       <Route index element={<HomePage />} />
       <Route path="/jobs" element={<JobsPage />} />
+      <Route
+        path="/jobs/:id"
+        element={<JobPage deleteJobSubmit={deleteJob} />}
+        loader={jobLoader}
+      />
       <Route path="*" element={<NotFoundPage />} />
+      <Route path="/add-job" element={<AddJobPage addJobSubmit={addJob} />} />
     </Route>,
   ),
 );
--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,5 +1,6 @@
 import JobListing from "./JobListing";
 import { useState, useEffect } from "react";
+import Spinner from "./Spinner";
 
 const JobListings = ({ isHome = false }) => {
   const [jobs, setJobs] = useState([]);
@@ -7,8 +8,9 @@ const JobListings = ({ isHome = false }) => {
 
   useEffect(() => {
     const fetchJobs = async () => {
+      const apiUrl = isHome ? "/api/jobs?_limit=3" : "/api/jobs";
       try {
-        const res = await fetch("http://localhost:5005/jobs");
+        const res = await fetch(apiUrl);
         const data = await res.json();
         setJobs(data);
       } catch (err) {
@@ -27,17 +29,15 @@ const JobListings = ({ isHome = false }) => {
         <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
           {isHome ? "Recent Jobs" : "Browse Jobs"}
         </h2>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-          {loading ? (
-            <h2>Loading</h2>
-          ) : (
-            <>
-              {jobs.map((job) => (
-                <JobListing key={job.id} job={job} />
-              ))}
-            </>
-          )}
-        </div>
+        {loading ? (
+          <Spinner loading={true} />
+        ) : (
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {jobs.map((job) => (
+              <JobListing key={job.id} job={job} />
+            ))}
+          </div>
+        )}
       </div>
     </section>
   );
